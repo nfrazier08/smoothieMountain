000a63193d0f96aa355e5f44a6e44198f4d6f18d
neliza <nicolasaelf64@gmail.com>
2017-10-30 00:15:15 -0600

    getting success from ajax, but post is not working

--- public/assets/clickFunctions.js
+++ public/assets/clickFunctions.js
@@ -3,22 +3,29 @@ $( document ).ready(function() {
  
 //Submit new smoothie 
 $("#smoothieButton").on("click", function(){
-        event.preventDefault();
-        var newSmoothieCreated = $("#createSmoothie").val().trim();
-        //THIS WORKS!!! GETTING CORRECT NEW SMOOTHIE CREATED!!
-        console.log(newSmoothieCreated);
+    event.preventDefault();
+    var newSmoothieDesired = $("#createSmoothie").val().trim();
 
+    //Pur created smoothie information into object
+    var createdSmoothieInfo = {
+        createdNewSmoothie: newSmoothieDesired
+    }
 
-// $.ajax({
-//             type: "POST",
-//             //Route for app.POST in apiRoutes
-//             url: "/api/smoothie",
-//             data: newSmoothie,
-//             success: 
-//             console.log()
-//             // location.reload()
-//     })
-})
+    //THIS WORKS!!! GETTING CORRECT NEW SMOOTHIE CREATED!!
+    console.log(createdSmoothieInfo);
+        $.ajax({
+            type: "POST",
+            //Route for app.POST in apiRoutes
+            url: "/api/smoothie",
+            data: createdSmoothieInfo,
+            success: 
+                //THIS WORKS! you have a problem with your API POST call!!!
+                console.log(createdSmoothieInfo)
+                //Eventually, I will want to reload the page, 
+                    //For now, check to see if you are getting the correct information upon AJAX success
+                // location.reload()
+        })
+}) //End of create new smoothie click function
 
 
 
